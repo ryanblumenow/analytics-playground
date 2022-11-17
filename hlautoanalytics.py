1093388167880982791efdbb04712317abe2d067
ryanblumenow <59917644+ryanblumenow@users.noreply.github.com>
2022-11-18 01:18:45 +0200

    Add files via upload

--- hlautoanalytics.py
+++ hlautoanalytics.py
@@ -71,7 +71,7 @@ from streamlit.components.v1 import html
 import streamlit.components.v1 as components
 import pandas as pd
 import streamlit as st
-from click_image_copy_from_demo import st_click_image
+import click_image_copy_from_demo
 from dtale.views import startup
 from streamlit_quill import st_quill
 # import pandas_profiling
@@ -162,7 +162,7 @@ class autoanalytics(HydraHeadApp):
                         st.markdown(text)
 
 
-        value = st_click_image()
+        value = click_image_copy_from_demo.st_click_image()
         if value is None:
             # st.stop()
             pass
